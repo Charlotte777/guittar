85111c7e38606cafe8edbd365c837f8b5500ba34
Charlotte777 <charlotte.acan@gmail.com>
2017-05-27 15:34:15 +0800

    song-list page

--- src/components/songlist/index.vue
+++ src/components/songlist/index.vue
@@ -1,23 +1,109 @@
 <template>
-  <div class="song-lists"> 
+  <div class="song-lists">
     <div class="list-header">
-        <div class="cover"><img src="/static/images/video1.jpg"></div>
-        <div class="info">
-            <h4>My youth is yours</h4>
-            <span>By</span><span>sda</span>
-            <h5>dasd</h5>
-        </div>
-    </div> 
-    <div class="song-list"></div>
+      <div class="cover"><img src="/static/images/video1.jpg"></div>
+      <div class="info">
+        <h2>{{ i.title }}</h2>
+        <h5 class="artist"><span>By</span>{{ i.artist }}</h5>
+        <h5><span class="s">简介:</span>{{ i.introduction }}</h5>
+        <h5><span>{{i.number}}</span>songs <guittar-button :todo="play"></guittar-button></h5>
+      </div>
+    </div>
+    <div class="song-list">
+      <lists v-for="i in songinfo" :todo="i" :key="i.number" ></lists>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  props:["todo"]
-}
+import guittarButton from '../general/guittar-button'
+import lists from './lists'
+  export default {
+    props: ["todo"],
+    data(){
+      return{
+        i:{
+          title:"My youth is yours",
+          artist:"啾啾啾-LH7",
+          introduction:"第一次听是小学四年级。印在脑中的场景是永远在拖地的教室和洁癖到偏执的班主任。时过境迁，还是没机会为你翘课，最后，勇气和那个下雨天一起消失。",
+          number:"51"
+        },
+        play:{
+          value:"播放全部"
+        },
+        songinfo:[{
+          number:'1.',
+          songtitle:"Shine",
+          album:'Communion (Deluxe)',
+          artist:"Years & Years",
+          time:"4:15"
+        },{
+          number:'1.',
+          songtitle:"Shine",
+          album:'Communion (Deluxe)',
+          artist:"Years & Years",
+          time:"4:15"
+        },{
+          number:'1.',
+          songtitle:"Shine",
+          album:'Communion (Deluxe)',
+          artist:"Years & Years",
+          time:"4:15"
+        },{
+          number:'1.',
+          songtitle:"Shine",
+          album:'Communion (Deluxe)',
+          artist:"Years & Years",
+          time:"4:15"
+        },{
+          number:'1.',
+          songtitle:"Shine",
+          album:'Communion (Deluxe)',
+          artist:"Years & Years",
+          time:"4:15"
+        },{
+          number:'1.',
+          songtitle:"Shine",
+          album:'Communion (Deluxe)',
+          artist:"Years & Years",
+          time:"4:15"
+        }]
+      }
+    },
+    components:{
+      guittarButton,
+      lists
+    }
+  }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  .song-lists .list-header {
+    display: inline-flex;
+    align-items: center;
+    margin: 10px 20px
+  }
+  .song-lists .list-header .cover {
+  }
+  .song-lists .list-header .cover img {
+    width:200px;
+    height: 200px
+  }
+  .song-lists .list-header .info {
+    padding: 0 20px;
+  }
+    .song-lists .list-header .info h5{
+      color:#828385;
+      font-weight:normal;
+      line-height:24px;
+      max-width:700px;
+    }
+    .song-lists .list-header .info h5 span{
+      color:#ffffff;
+      margin-right: 5px;
+    }
+  .song-lists .list-header .info .artist {
+    cursor:pointer;
+  }
 </style>
---
+++ src/components/songlist/lists.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="lists">
+    <div class="number"><span>{{ todo.number }}</span></div>
+    <div class="song-info">
+      <p class="songtitle">{{ todo.songtitle }}</p>
+      <p class="album"><span style="margin-right: 5px">{{todo.artist}}</span>-<span style="margin-left: 5px">{{ todo.album }}</span></p>
+    </div>
+    <div class="time">{{ todo.time }}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ["todo"]
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .lists {
+    align-items: center;
+    color: #aaa9a9;
+    display: flex;
+    padding-bottom: 10px;
+  }
+  .lists .number {
+    flex-grow: 2;
+    text-align: center;
+  }
+  .lists .song-info {
+    flex-grow: 6
+  }
+  .lists .song-info .songtitle {
+    color: #fff;
+    font-size: 18px;
+  }
+  .lists .song-info .album {
+    font-size: 14px;
+  }
+  .lists .time {
+    flex-grow: 2
+  }
+</style>

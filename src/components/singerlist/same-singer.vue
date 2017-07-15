d0e0a7c7bb986511fc2b273487f4cb581e56fe66
Charlotte <charlotte.acan@gmail.com>
2017-07-15 23:51:02 +0800

    en

--- src/components/singerlist/same-singer.vue
+++ src/components/singerlist/same-singer.vue
@@ -1,8 +1,25 @@
 <template>
-  <div class="same-singer">相似歌手</div>
+  <div class="same-singer">
+    <guittar-list v-for="i in list" :todo="i" :key="i.title"></guittar-list>
+  </div>
 </template>
 
 <script>
+import guittarList from '../general/guittar-list'
+export default{
+data(){
+  return{
+    list:[{
+      img:"static/images/lu.jpg",
+      title:"xu",
+      name:"da"
+    }]
+  }
+},
+  component:{
+    guittarList
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
--- src/components/singerlist/singer-info.vue
+++ src/components/singerlist/singer-info.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="same-singer">歌手详情</div>
+  <div class="singer-info">
+    <h3>周杰伦简介</h3>
+    <span>著名歌手，音乐人，词曲创作人，编曲及制作人，MV及电影导演。新世纪华语歌坛领军人物，中国风歌曲始祖，被时代周刊誉为“亚洲猫王”，是2000年后亚洲流行乐坛最具革命性与指标性的创作歌手，亚洲销量超过3100万张，有“亚洲流行天王”之称，开启华语乐坛“R&B时代”与“流行乐中国风”的先河，周杰伦的出现打破了亚洲流行乐坛长年停滞不前的局面，为亚洲流行乐坛翻开了新的一页，是华语乐坛真正把R&B提升到主流高度的人物，引领华语乐坛革命整十年，改写了华语乐坛的流行方向。</span>
+  </div>
 </template>
 
 <script>
@@ -7,4 +10,11 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.singer-info{
+  margin-top: 20px;
+}
+.singer-info h3{
+    color: #f3f3f3;
+    letter-spacing: 1px;
+    padding-bottom: 10px;}
 </style>
